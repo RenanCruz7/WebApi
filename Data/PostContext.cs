65eeb1d394d568a8c40950466a1e7b04c2fa91d6
renan <renancruz199@gmail.com>
2024-07-23 19:14:22 -0300

    Realizando primeira migration

--- Data/PostContext.cs
+++ Data/PostContext.cs
@@ -4,6 +4,10 @@
  * Com DbContext, podemos criar um contexto de banco de dados para o Entity Framework Core.
  * o que facilita a conexão com o banco de dados e a manipulação de dados.
  * então toda vez que quisermos acessar o banco de dados, podemos usar o contexto.
+ * 
+ * Lembrando que para fazer isso preciso instalar o pacote do Entity Framework Core. 
+ * e o pacote do MySql para o Entity Framework Core.
+ * 
  **/
 
 
--- Models/Post.cs
+++ Models/Post.cs
@@ -8,6 +8,7 @@ public class Post
     [Required]
     public int Id { get; set; }
     [Required(ErrorMessage = "O Titulo é obrigatorio")]
+    [MaxLength(50, ErrorMessage = "O tamanho do Titulo não pode exceder 50 caracteres")]
     public string Title { get; set; }
     [Required(ErrorMessage = "O Conteudo é obrigatorio")]
     public string Content { get; set; }
